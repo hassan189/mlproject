0a6c6a721a09b1d6e08bb3ab63d8fbb65f7b252b
hassan189 <hassan.abdul189@gmail.com>
2024-07-07 19:23:12 +0530

    Hyperparameter tunning

--- src/utils.py
+++ src/utils.py
@@ -1,28 +1,40 @@
+
 import os
 import sys
-import numpy as np
+
+import numpy as np 
 import pandas as pd
-from src.exception import CustomException
 import dill
+import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
-def save_object(file_path,obj):
+from src.exception import CustomException
+
+def save_object(file_path, obj):
     try:
-        dir_path=os.path.dirname(file_path)
-        os.makedirs(dir_path,exist_ok=True)
+        dir_path = os.path.dirname(file_path)
 
-        with open(file_path,"wb") as file_obj:
-            dill.dump(obj,file_obj)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
-        raise CustomException(e,sys)
-
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
@@ -38,6 +50,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
